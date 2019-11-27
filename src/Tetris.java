89c5dfd4b6e25c2cdfb3ac7bf941be4de0151b37
Linus Brogan <git@linusaur.us>
2019-11-27 13:19:48 -0800

    Initial commit

---
+++ src/Tetris.java
@@ -0,0 +1,15 @@
+public class Tetris {
+    public static void main(String[] args) {
+        new Tetris().run();
+    }
+
+    public void run() {
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(); // Set scale
+        while (true) {
+            StdDraw.pause(10);
+            //get input
+            //draw
+        }
+    }
+}
